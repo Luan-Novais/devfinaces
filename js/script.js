3780a3b46f6614588d4045ef2231dee90ae6596f
Luan-Novais <luan.novaiss@outlook.com>
2021-02-05 08:39:28 -0300

    Adicionado funcao modal

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#novaTransacao').addEventListener("click",function(e){
+    document.querySelector('.modal-overlay').classList.add('active')
+});
+
+
+document.querySelector("#cancelar").addEventListener("click", function(e){
+    e.preventDefault()
+    document.querySelector('.modal-overlay').classList.remove('active')
+});
