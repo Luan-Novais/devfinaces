f301365f8418d0d85a8d22130ad5c9f90c42962a
Luan-Novais <luan.novaiss@outlook.com>
2021-02-08 22:45:41 -0300

    Iniciando JS

--- js/script.js
+++ js/script.js
@@ -8,3 +8,64 @@ document.querySelector("#cancelar").addEventListener("click", function(e){
     e.preventDefault();
     document.querySelector('.modal-overlay').classList.remove('active')
 });
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 50000,
+        date: '23/01/2021',
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    },
+]
+
+const Transaction = {
+    incomes() {
+
+    },
+    expenses() {
+
+    },
+    total() {
+
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction,index) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+        DOM.transactionsContainer.appendChild(tr);
+
+        console.log(tr);
+    },
+
+    innerHTMLTransaction(transaction) {
+        
+        const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="expense">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td class="minus"> <a href="#">
+            <img src="./assets/minus.svg" alt=""></a>
+        </td>
+        `
+
+        return html
+    }
+}
+
+DOM.addTransaction(transactions[0]);
